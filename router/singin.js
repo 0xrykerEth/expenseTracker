4788b62a9deb52b8af94d9acb3a0488fdd8d13a7
oxryker.bnb <86192937+0xrykerEth@users.noreply.github.com>
2025-02-13 20:02:14 +0530

    added login page
    added login page to check credentials

--- router/singin.js
+++ router/singin.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
+const Expense = require('../models/data')
 
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'exist.html'));
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await Expense.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        if (user.password !== password) {
+            return res.status(400).json({ error: "Incorrect password" });
+        }
+
+        res.status(200).json({ message: "Logged in successfully!" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 module.exports = router;
